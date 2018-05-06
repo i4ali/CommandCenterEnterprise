0ed980f5ef02f7de3c9a95ac0dacb5c37020b35b
imran <ali.muhammadimran@gmail.com>
2018-05-06 01:33:38 -0400

    add test suites

---
+++ Test Suites/TestSuite-Login.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite-Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-06T00:58:52</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>aa306836-2ad7-48d1-a956-9c49acbfc12f</testSuiteGuid>
+   <testCaseLink>
+      <guid>6f3c7861-1b68-4c1e-b4eb-92a078c70c0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UnsuccessfulLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e5c0ee45-81bd-44f1-9c47-80a8a4c34643</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SuccessfulLogin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
